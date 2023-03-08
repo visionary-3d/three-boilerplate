8ea9c00c53cf0fc19303cd212ec99c12dfa29e99
arya <64514807+arya3d@users.noreply.github.com>
2023-03-08 22:22:19 +0330

    Add mp3 and wav support

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -52,7 +52,7 @@ module.exports = {
       { test: /\.js$/, use: ['babel-loader'] },
 
       // Images: Copy image files to build folder
-      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+      { test: /\.(?:ico|gif|png|jpg|jpeg|mp3|wav)$/i, type: 'asset/resource' },
 
       // Fonts and SVGs: Inline files
       { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
